c53a82a77328cb7115c4ff786b00e2a5c42e3548
Leonardo Nazario de Moraes <leonardonm_bio@hotmail.com>
2021-10-01 00:09:44 -0300

    alterar acesso ajax

--- GerenciamentoAmostras/_amostras.php
+++ GerenciamentoAmostras/_amostras.php
@@ -289,8 +289,7 @@
 		</form>
 		</fieldset>
 		
-		<script type="text/javascript" src="https://www.google.com/jsapi"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+		<script type="text/javascript" src="_codigos/jquery.min.js"></script>
 		
 		<script type="text/javascript">
             // seleção da amostra
